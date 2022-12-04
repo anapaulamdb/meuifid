163cf85f3395de9c756e22287f4b4a6a993f05aa
anapaulamdb <anapaulamdb@gmail.com>
2022-12-04 19:52:12 -0300

    add imagem ao banco

--- app/src/main/java/com/example/meuid/activityViews/TelaPrincipal.java
+++ app/src/main/java/com/example/meuid/activityViews/TelaPrincipal.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.meuid.R;
 import com.example.meuid.database.DBHelper;
@@ -25,8 +29,9 @@ public class TelaPrincipal extends AppCompatActivity {
     TextView textCPF;
     TextView textUnidade;
     TextView textValidade;
-    String codigo;
+    Integer codigo;
     Cursor cursor;
+    byte[] imageBytes;
 
 
 
@@ -45,23 +50,57 @@ public class TelaPrincipal extends AppCompatActivity {
         textCPF = (TextView) findViewById(R.id.textCPF);
         textUnidade = (TextView) findViewById(R.id.textUnidade);
         textValidade = (TextView) findViewById(R.id.textValidade);
+        imageView = (ImageView) findViewById(R.id.imageView);
 
+        DBHelper db = new DBHelper(getBaseContext());
+        Intent intent = getIntent();
+        int temp = intent.getIntExtra("codigo", 0);
+        cursor = db.carregaDadoById(temp);
 
-        codigo = this.getIntent().getStringExtra("4");
+        String texto = cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.IMAGEM));
 
-        DBHelper crud = new DBHelper(getBaseContext());
+        byte[] decodedString = Base64.decode(texto.getBytes(), Base64.NO_WRAP);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
-        //alterar = (Button)findViewById(R.id.button2);
-
-        cursor = crud.carregaDadoById(Integer.parseInt(codigo));
         textNome.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.NOME)));
         textCurso.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.CURSO)));
         textMatricula.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.MATRICULA)));
         textCPF.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.CPF)));
         textUnidade.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.CAMPUS)));
+        imageView.setImageBitmap(decodedByte);
+
+        //imageView.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.CAMPUS)));
        // textValidade.setText(cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.)));
+        textValidade.setText("00/00/0000");
+        //imageView.setImageDrawable(foto);
 
+        /*
 
+        Intent intent = getIntent();
+        int temp = intent.getIntExtra("int_value", 0);
+        //alterar = (Button)findViewById(R.id.button2);
+
+        cursor = crud.carregaDadoById(temp);
+        textNome
+        -----
+
+        codigo = this.getIntent().getStringExtra("codigo");
+
+        crud = new BancoController(getBaseContext());
+
+        livro = (EditText)findViewById(R.id.editText4);
+        autor = (EditText)findViewById(R.id.editText5);
+        editora = (EditText)findViewById(R.id.editText6);
+
+        alterar = (Button)findViewById(R.id.button2);
+
+        cursor = crud.carregaDadoById(Integer.parseInt(codigo));
+        livro.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.TITULO)));
+        autor.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.AUTOR)));
+        editora.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.EDITORA)));
+
+    }
+         */
         code_qr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,3 +123,34 @@ public class TelaPrincipal extends AppCompatActivity {
 
 
 }
+/*
+
+        //alterar = (Button)findViewById(R.id.button2);
+
+
+        textNome
+        -----
+
+        codigo = this.getIntent().getStringExtra("codigo");
+
+        Bitmap raw;
+        byte[] fotoArray = cursor.getColumnIndexOrThrow(DBHelper.IMAGEM);
+        ImageView foto = imageView.findViewById(R.id.imageView);
+        if(fotoArray!=null) {
+            raw  = BitmapFactory.decodeByteArray(fotoArray,0,fotoArray.length);
+            foto.setImageBitmap(raw);
+        }
+
+
+         //String imagemBanco = cursor.getString(cursor.getColumnIndexOrThrow(DBHelper.IMAGEM));
+        //imageBytes = Base64.decode(imagemBanco, Base64.DEFAULT);
+       // Bitmap imagemDecode = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        //imageView.setImageBitmap(imagemDecode);
+
+
+                           Bitmap bitmap = ((BitmapDrawable)imgSelected.getDrawable()).getBitmap();
+                    ByteArrayOutputStream saida = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG,100,saida);
+                    img = saida.toByteArray();
+                    String fotoSaida = Base64.encodeToString(img, Base64.DEFAULT);
+ */
